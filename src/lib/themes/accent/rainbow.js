53630f4c10d87d19cb00da95aadd3d8ddf3bc75f
LilyMakesThings <127533508+LilyMakesThings@users.noreply.github.com>
2024-06-01 18:56:48 +0100

    Create rainbow.js
    this is ported directly from Unsandboxed, so the colours might need tweaking

---
+++ src/lib/themes/accent/rainbow.js
@@ -0,0 +1,37 @@
+const guiColors = {
+    'motion-primary': '#66757f',
+    'motion-primary-transparent': '#667580e6',
+    'motion-tertiary': '#4a5a65',
+
+    'looks-secondary': '#66757f',
+    'looks-transparent': '#66758059',
+    'looks-light-transparent': '#66758026',
+    'looks-secondary-dark': 'hsla(204, 8%, 35%, 1)',
+
+    'extensions-primary': 'hsla(205, 22%, 65%, 1)',
+    'extensions-tertiary': 'hsla(203, 16%, 22%, 1)',
+    'extensions-transparent': 'hsla(204, 19%, 67%, 0.35)',
+    'extensions-light': 'hsla(203, 16%, 84%, 1)',
+
+    'drop-highlight': '#8ea3b2',
+
+    'menu-bar-alternate': `linear-gradient(
+        90deg,
+        rgba(255, 76, 76, 0.7) 0%,
+        rgba(255, 140, 26, 0.7) 20%,
+        rgba(255, 191, 0, 0.7) 40%,
+        rgba(89, 192, 89, 0.7) 60%,
+        rgba(76, 151, 255, 0.7) 80%,
+        rgba(153, 102, 255, 0.7) 100%
+    )`
+};
+
+const blockColors = {
+    checkboxActiveBackground: '#66757f',
+    checkboxActiveBorder: '#4a5a65'
+};
+
+export {
+    guiColors,
+    blockColors
+};
